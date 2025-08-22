0ac56412b7ed1f8fbc984ad75cae5021b4aa032a
ivannissimrch <ivannissimrch@gmail.com>
2025-08-21 21:46:30 -0400

    refactor server code, split code in multiple files

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import router from "./router.js";
+import { protect } from "./auth.js";
+import { JSONFilePreset } from "lowdb/node";
+
+const app = express();
+
+// Initialize database
+const dbPath =
+  process.env.NODE_ENV === "production" ? "/data/db.json" : "db.json";
+const db = await JSONFilePreset("db.json", { users: [] });
+if (!db.data.users) {
+  db.data.users = [];
+}
+
+// Middleware
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+
+app.use("/api", protect, router);
+app.use("/");
+
+export default app;
+export { db };
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,6 @@
 import styles from "./page.module.css";
-import Link from "next/link";
-import { User } from "./Types";
 
 export default async function Login() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users`);
-  const data = await response.json();
-  const users: User[] = data.data;
-
   return (
     <main className={styles.main}>
       <div className={styles.container}>
@@ -15,15 +9,25 @@ export default async function Login() {
           Master English pronunciation through shadowing practice
         </p>
         <div className={styles.buttonsContainer}>
-          {users.map((user) => (
-            <Link
-              key={user.name}
-              href={`/${user.name}`}
-              className={styles.button}
-            >
-              {user.name}
-            </Link>
-          ))}
+          <form className={styles.form}>
+            <input
+              type="text"
+              name="username"
+              placeholder="Enter your name"
+              className={styles.input}
+              required
+            />
+            <input
+              type="text"
+              name="password"
+              placeholder="Enter your password"
+              className={styles.input}
+              required
+            />
+            <button type="submit" className={styles.button}>
+              Login
+            </button>
+          </form>
         </div>
       </div>
     </main>
