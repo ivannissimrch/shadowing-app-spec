fdccfd0a8b9a61b02509d0c4585ceb5358d119a0
ivannissimrch <ivannissimrch@gmail.com>
2025-08-24 22:35:35 -0400

    remove protected

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
-app.use("/api", protect, router);
+app.use("/api", router);
 app.post("/signin", signin);
 
 export default app;
