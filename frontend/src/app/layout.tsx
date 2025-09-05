1a0cf877bb506d1c9241551945ed2236d58396b4
ivannissimrch <ivannissimrch@gmail.com>
2025-09-04 21:22:06 -0400

    Remove footer and header from global layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AppContext from "./AppContext";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import AlertDialog from "./components/AlertDialog";
 
 const geistSans = Geist({
@@ -31,9 +29,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <AppContext>
           <>
-            <Header />
             {children}
-            <Footer />
             <AlertDialog />
           </>
         </AppContext>
